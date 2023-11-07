806781e8254f66e4b900be53f5c2916db93e84bf
NileshMandole <july23.nileshmandole@gmail.com>
2023-11-07 22:15:52 +0530

    commit by nileshmandole

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,8 @@
-package com.myjava;
+package com.accessmodifires;
 
 public class HelloWorld {
-
-    //Java is a case sensitive language public -> public are two different things
-    public static void main (String [] args) {
-        System.out.println("I am learning Java.. My name is Nilesh");
-        System.out.println("I am a singer");
+    public static void main(String[] args) {
+        System.out.println("I am learning Java");
     }
 }
+
