08c2f733320ff578039931b436dac7dc448ac211
NileshMandole <july23.nileshmandole@gmail.com>
2023-11-07 21:57:14 +0530

    Commit by nilesh

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.myjava;
+
+public class HelloWorld {
+
+    //Java is a case sensitive language public -> public are two different things
+    public static void main (String [] args) {
+        System.out.println("I am learning Java.. My name is Nilesh");
+        System.out.println("I am a singer");
+    }
+}
