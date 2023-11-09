5c491118cf973be0eb23c13412587dcf1cf6c204
Pankaj B <pankajbhambhani01@gmail.com>
2023-11-09 09:01:32 +0530

    my first automated testcase

---
+++ src/test/java/com/testcases/TestCase1.java
@@ -0,0 +1,30 @@
+package com.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class TestCase1 {
+    public static void main(String[] args) throws Exception {
+        //https://chromedriver.chromium.org/downloads
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Pankaj Bhambhani/Downloads/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(5000);// 5 sec wait
+        WebElement element;
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("Admin");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("admin123");
+    }
+}
+// DOM - document object model (HTML model) - Hyper text mark up language source
+//<tagName attributes=value > -- </tagName>
+// locator identifiers
+// name
+//Assignments -> naviagete to Parabank website
+// enter any text in username and password field
