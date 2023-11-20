471cf349ad88e650a17ce07e14adf1a43f7bc57b
Pankaj B <pankajbhambhani01@gmail.com>
2023-11-20 08:53:31 +0530

    20_11_2023

--- src/test/java/com/pages/swaglab/pages.java
+++ src/test/java/com/pages/swaglab/pages.java
@@ -1,9 +1,10 @@
 package com.pages.swaglab;
 
 public class pages {
-   
+
     // Login Page -> Ritvik -> LoginPage.java
     // Left Navigation Page -> Datta -> LeftNavigationPage.java
     // Home Page -> Nilesh
     // cart page ->
+    //
 }
