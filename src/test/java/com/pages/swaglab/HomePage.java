a695cf368df7d075581f506880a18258c5e0eff3
NileshMandole <july23.nileshmandole@gmail.com>
2023-11-23 12:30:36 +0530

    my first file

--- src/test/java/com/pages/swaglab/HomePage.java
+++ src/test/java/com/pages/swaglab/HomePage.java
@@ -3,30 +3,27 @@ package com.pages.swaglab;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class HomePage {
-    public static void main(String[]args) throws Exception{
-        System.setProperty("webdriver.chrome.driver","C:/Program Files/chromedriver-win64/chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+    @Test
+    public void HomePage() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/v1/inventory.html");
         driver.manage().window().maximize();
-        Thread.sleep(3000);
-
-
-        driver.findElement(By.className("product_sort_container")).click();
+        Thread.sleep(4000);
         driver.findElement(By.xpath("//option[text()='Name (A to Z)']")).click();
-
-        driver.findElement(By.className("product_sort_container")).click();
+        System.out.println("Products are sorted as A to z ");
         driver.findElement(By.xpath("//option[text()='Name (Z to A)']")).click();
+        System.out.println("Products are sorted as Z to A");
+        driver.findElement(By.xpath("//option[text()='Price (low to high)']")).click();
+        System.out.println("As per price low to high");
+        driver.findElement(By.xpath("//option[text()='Price (high to low)']")).click();
+        System.out.println("As per price high to low");
 
-        driver.findElement(By.className("product_sort_container")).click();
-        driver.findElement(By.xpath("//option[text()='Name (low to high)']")).click();
-
-        driver.findElement(By.className("product_sort_container")).click();
-        driver.findElement(By.xpath("//option[text()='Name (high to low)']")).click();
-
-        driver.quit();
     }
 
-
 }
+
