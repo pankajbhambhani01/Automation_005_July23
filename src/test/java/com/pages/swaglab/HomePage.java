9077c0afc3bd442f3df1341e6c21464271060168
NileshMandole <july23.nileshmandole@gmail.com>
2023-11-25 09:05:21 +0530

    HomePage

--- src/test/java/com/pages/swaglab/HomePage.java
+++ src/test/java/com/pages/swaglab/HomePage.java
@@ -2,28 +2,42 @@ package com.pages.swaglab;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+
 
 public class HomePage {
-    @Test
-    public void HomePage() throws Exception {
 
-        System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/v1/inventory.html");
+    private WebDriver driver;
+
+    public By sortNameAtoZ = By.xpath("//option[text()='Name (A to Z)']");
+    public By sortNameZtoA = By.xpath("//option[text()='Name (Z to A)']");
+    public By sortPriceLowHigh = By.xpath("//option[text()='Price (low to high)']");
+    public By sortPriceHighLow = By.xpath("//option[text()='Price (high to low)']");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openHomepage(String url) {
+        driver.get(url);
         driver.manage().window().maximize();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("//option[text()='Name (A to Z)']")).click();
-        System.out.println("Products are sorted as A to z ");
-        driver.findElement(By.xpath("//option[text()='Name (Z to A)']")).click();
-        System.out.println("Products are sorted as Z to A");
-        driver.findElement(By.xpath("//option[text()='Price (low to high)']")).click();
-        System.out.println("As per price low to high");
-        driver.findElement(By.xpath("//option[text()='Price (high to low)']")).click();
-        System.out.println("As per price high to low");
+    }
 
+    public void sortProductsAtoZ() {
+        driver.findElement(sortNameAtoZ).click();
+    }
+
+    public void sortProductsZtoA() {
+        driver.findElement(sortNameZtoA).click();
+    }
+
+    public void sortProductsPriceLowHigh() {
+        driver.findElement(sortPriceLowHigh).click();
+    }
+
+    public void sortProductsPriceHighLow() {
+        driver.findElement(sortPriceHighLow).click();
     }
 
 }
+
 
