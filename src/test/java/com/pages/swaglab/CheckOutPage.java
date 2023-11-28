40b3cf6f5cd90b7533e37aca1aea84732131df93
Shahabaz <july23.shahabaz@gmail.com>
2023-11-28 21:57:16 +0530

    CheckOut page with page factory

--- src/test/java/com/pages/swaglab/CheckOutPage.java
+++ src/test/java/com/pages/swaglab/CheckOutPage.java
@@ -3,38 +3,60 @@ package com.pages.swaglab;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class CheckOutPage {
 
     private WebDriver driver;
-    public CheckOutPage(WebDriver driver) {
+    @FindBy(xpath = "//*[@id='first-name' and @placeholder='First Name']")
+    private WebElement txt_FirstName;
+
+    @FindBy(xpath = "//*[@id='last-name' and @placeholder='Last Name']")
+    private WebElement txt_LastName;
+
+    @FindBy(xpath = "//*[@id='postal-code' and @placeholder='Zip/Postal Code']")
+    private WebElement txt_ZipCode;
+
+    @FindBy(xpath = "//*[contains(@class,'btn_secondary')]")
+    private WebElement btn_Cancel;
+
+    @FindBy(xpath = "//*[contains(@class,'cart_button')]")
+    private WebElement btn_Continue;
+
+    @FindBy(xpath = "//*[starts-with(@class,'bm-burger-button')]")
+    private WebElement btn_Menu;
+
+    @FindBy(xpath = "//*[starts-with(@class,'svg-inline--fa')]")
+    private WebElement btn_CheckOutbtn;
+
+    public CheckOutPage(WebDriver driver){
         this.driver = driver;
+        PageFactory.initElements(driver,this);
     }
-    public void enterFirstName(String FirstName) {
-        WebElement element = driver.findElement(By.xpath("//*[@id='first-name' and @placeholder='First Name']"));
-        element.sendKeys(FirstName);
+
+    public void enterFirstName(String firstname){
+        txt_FirstName.sendKeys(firstname);
     }
-    public void enterLastName(String LastName) {
-        WebElement element = driver.findElement(By.xpath("//*[@id='last-name' and @placeholder='Last Name']"));
-        element.sendKeys(LastName);
-    }
-    public void enterPostalCode(String ZipCode) {
-        WebElement element = driver.findElement(By.xpath("//*[@id='postal-code' and @placeholder='Zip/Postal Code']"));
-        element.sendKeys(ZipCode);
-    }
+
+    public void enterLastName(String lastname){  txt_LastName.sendKeys(lastname); }
+
+    public void enterZipCode(String zipcode){  txt_ZipCode.sendKeys(zipcode); }
+
     public void clickCancel(){
-        WebElement element = driver.findElement(By.xpath("//*[contains(@class,'btn_secondary')]"));
-        element.click();
+        btn_Cancel.click();
     }
+
     public void clickContinue(){
-        WebElement element = driver.findElement(By.xpath("//*[contains(@class,'cart_button')]"));
-        element.click();
+        btn_Continue.click();
     }
-}
 
+    public void clickMenu(){ btn_Continue.click(); }
 
-               //...
-        //*[contains(@class,'footer_robot')]
+    public void clickCheckOutbtn(){ btn_CheckOutbtn.click(); }
+    }
+
+    
         //*[contains(@class,'cart_button')]
         //*[contains(@class,'btn_secondary')]
         //driver.findElement(By.xpath("//*[@id='first-name' or @placeholder='First Name']")).click();
