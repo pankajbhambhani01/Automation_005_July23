a3a3910d1cb18324dd6e6b8c7d0123667f69a13f
NileshMandole <july23.nileshmandole@gmail.com>
2023-11-29 08:07:01 +0530

    HomePage

--- src/test/java/com/pages/swaglab/HomePage.java
+++ src/test/java/com/pages/swaglab/HomePage.java
@@ -1,44 +1,57 @@
 package com.pages.swaglab;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 public class HomePage {
 
     private WebDriver driver;
 
-    private By sortNameAtoZ = By.xpath("//option[text()='Name (A to Z)']");
-    private By sortNameZtoA = By.xpath("//option[text()='Name (Z to A)']");
-    private By sortPriceLowHigh = By.xpath("//option[text()='Price (low to high)']");
-    private By sortPriceHighLow = By.xpath("//option[text()='Price (high to low)']");
+    @FindBy(xpath = "//option[text()='Name (A to Z)']")
+    private WebElement sortNameAtoZ;
+
+    @FindBy(xpath = "//option[text()='Name (Z to A)']")
+    private WebElement sortNameZtoA;
+
+    @FindBy(xpath = "//option[text()='Price (low to high)']")
+    private WebElement sortPriceLowHigh;
+
+    @FindBy(xpath = "//option[text()='Price (high to low)']")
+    private WebElement sortPriceHighLow;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    public void clickButtonToCart(String product){
+    public void clickButtonToCart(String product) {
         WebElement element = driver.findElement(By.xpath("//*[contains(@class,'inventory_item_name') and text() = '" + product + "']//ancestor::div[@class='inventory_item']//button"));
         element.click();
     }
 
-    public void validateButton(String product, String button){
+    public void validateButton(String product, String button) {
         WebElement element = driver.findElement(By.xpath("//*[contains(@class,'inventory_item_name') and text() = '" + product + "']//ancestor::div[@class='inventory_item']//button"));
         String expectedButtonText = element.getText();
         Assert.assertEquals(expectedButtonText, button);
     }
 
-    public void validatePrice(String product, String price){
+    public void validatePrice(String product, String price) {
         WebElement element = driver.findElement(By.xpath("//*[contains(@class,'inventory_item_name') and text() = '" + product + "']//ancestor::div[@class='inventory_item']//div[@class='inventory_item_price']"));
         String actualPrice = element.getText();
         Assert.assertEquals(actualPrice, price);
     }
 
-    public void validateDetails(String product, String description){
+    public void validateDetails(String product, String description) {
         WebElement element = driver.findElement(By.xpath("//*[contains(@class,'inventory_item_name') and text() = '" + product + "']//ancestor::div[@class='inventory_item']//div[@class='inventory_item_desc']"));
         String actualDescription = element.getText();
         Assert.assertEquals(actualDescription, description);
     }
+
+
 
 }
 
